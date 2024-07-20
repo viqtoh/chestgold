48c9e583bf1c47b084181fc05fa469ee131fba0a
Victor Aromiwe <76104252+viqtoh@users.noreply.github.com>
2024-07-20 20:03:47 +0100

    fixed static files

--- chestgold/urls.py
+++ chestgold/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf.urls.static import static
 from . import settings
 from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("user.urls")),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-]
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS[0]}),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
