7fb25035468e8a11c7b63a865300f7107f307c1b
Victor Aromiwe <76104252+viqtoh@users.noreply.github.com>
2024-08-06 12:15:52 +0100

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,7 +10,7 @@ if project_home not in sys.path:
 os.environ['DJANGO_SETTINGS_MODULE'] = 'chestgold.settings'
 
 # Activate the virtual environment
-activate_this = '/home2/chestgol/virtualenv/chestgold/3.9/bin/activate_this.py'
+activate_this = '/home2/virtualenv/chestgold/3.9/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
